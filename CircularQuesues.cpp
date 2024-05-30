d7807f61eae7eecbcdb31cb7e4cd7484ae1cfbd2
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-05-30 13:17:53 +0700

    Membuat prosedur remove mengecek apakah antrian kosong

--- CircularQuesues.cpp
+++ CircularQuesues.cpp
@@ -39,3 +39,11 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue underflow\n";
+			return;
+		}
+		cout << "\nThe element delete from the queue is :" << queue_array[FRONT] << "\N";
