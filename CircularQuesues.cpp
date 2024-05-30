1dd4de4c183e52a88e381161d12b3acf84400767
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-05-30 10:35:31 +0700

    Membuat kelas queues

--- CircularQuesues.cpp
+++ CircularQuesues.cpp
@@ -4,24 +4,4 @@ using namespace std;
 //CircularQuesuesArray_0190
 // Min commit = 15
 class queues {
-	int FRONT, REAR, max = 5;
-	int queue_array[5];
-
-public:
-	queues() {
-		FRONT = -1;
-		REAR = -1;
-	}
-
-	void insert() {
-		int num;
-		cout << "Enter a number : ";
-		cin >> num;
-		cout << endl;
-
-		//cek apakah antrian penuh
-		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-
-		}
- 	}
-};
+	
